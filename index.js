911c3d591c3b878e85e123240cb0a023e113e39c
Gagan021-5 <gagansingh010221@gmail.com>
2025-03-16 22:57:17 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const  a = document.querySelector("input");
+const b = document.querySelector("button");
+const c = document.createElement("div");
+const cont = document.querySelector(".after");
+
+cont.style.display ="none";
+
+async function get(){
+    let response = await fetch(`http://api.weatherapi.com/v1/current.json?key=9ccfaec91a3b45eab17160641251503&q=${a.value}&aqi=no`);
+    let data = await response.json();
+
+
+    cont.style.display ="block";
+    cont.innerHTML =`<img src ="${data.current.condition.icon}" alt="Weather icon">
+             <p><strong>Location:</strong> ${data.location.name}, ${data.location.country}</p>
+            <p><strong>Condition:</strong> ${data.current.condition.text}</p>
+            <p><strong>Temperature:</strong> ${data.current.temp_c}°C</p>`;
+   console.log(data);
+   
+}
+
+a.addEventListener("focus",()=>{
+    a.value= " ";  
+
+})
+
+b.addEventListener("click", get); 
+
+
+
+
+
+
